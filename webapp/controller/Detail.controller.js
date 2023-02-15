31e44a4a49289e2ce7ea06d07db50e1416814ce2
EnricoFerro <enricoferrospam@gmail.com>
2023-02-15 22:34:58 +0100

    Here the changes for W2.2 (#1)

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -231,26 +231,55 @@ sap.ui.define([
            _onApproveToCAPM: function(){
                 var that = this;
                 var oBindingCtx = that.getView().getBindingContext("LocalModel").getObject();
-                var oPayload = {
-                    status  : oBindingCtx.selNextApprover.split('|')[1],
-                    nextApprover    : oBindingCtx.selNextApprover.split('|')[0],
-                };
-                var sUrl = sServiceUrl + "PurchaceReqSet/"+oBindingCtx.id;
-                this.loadBusyIndicator("ObjectPageLayout", true);
-                ReqHelper.sendUpdateReq(sUrl, oPayload).then(function (oRes) {
-                    that.loadBusyIndicator("ObjectPageLayout",false);
-                    //that.oCommentsDlg.close();
-                    that._postComments(oRes);
-                  //  that._postHistory(oRes);
-                    MessageBox.success('Approved',{
-                        onClose: function () {
-                            that.getRouter().navTo("main");
-                        }.bind(that)
-                    });
-                }.bind(this))
-                .catch(function (response) {
-                    that.loadBusyIndicator("ObjectPageLayout",false);
-                }.bind(this));
+                if (oBindingCtx.id !== undefined ) {
+                    var oPayload = {
+                        status  : oBindingCtx.selNextApprover.split('|')[1],
+                        nextApprover    : oBindingCtx.selNextApprover.split('|')[0],
+                    };
+                    var sUrl = sServiceUrl + "PurchaceReqSet/"+oBindingCtx.id;
+                    this.loadBusyIndicator("ObjectPageLayout", true);
+                    ReqHelper.sendUpdateReq(sUrl, oPayload).then(function (oRes) {
+                        that.loadBusyIndicator("ObjectPageLayout",false);
+                        //that.oCommentsDlg.close();
+                        that._postComments(oRes);
+                      //  that._postHistory(oRes);
+                        MessageBox.success('Approved',{
+                            onClose: function () {
+                                that.getRouter().navTo("main");
+                            }.bind(that)
+                        });
+                    }.bind(this))
+                    .catch(function (response) {
+                        that.loadBusyIndicator("ObjectPageLayout",false);
+                    }.bind(this));
+                } else {
+                    var oLocalModel = that.getModel("LocalModel");
+                    var sRequestor = oLocalModel.getProperty("/LoggedInUserID");
+                    var oPayload = {
+                        pr  : oBindingCtx.Banfn,
+                        status  : oBindingCtx.selNextApprover.split('|')[1],
+                        nextApprover    : oBindingCtx.selNextApprover.split('|')[0],
+                        requestor   : sRequestor
+                    };
+                    var sUrl = sServiceUrl + "PurchaceReqSet";
+                    this.loadBusyIndicator("ObjectPageLayout", true);
+                    
+                    ReqHelper.sendCreateReq(sUrl, oPayload).then(function (oRes) {
+                        that.loadBusyIndicator("ObjectPageLayout",false);
+                        //that.oCommentsDlg.close();
+                        that._postComments(oRes);
+                        //that._postHistory(oRes);
+                        MessageBox.success('Approved',{
+                            onClose: function () {
+                                that.getRouter().navTo("main");
+                            }.bind(that)
+                        });
+                    }.bind(this))
+                    .catch(function (response) {
+                        that.loadBusyIndicator("ObjectPageLayout",false);
+                    }.bind(this));
+                }
+
             },/**
             * Method to submit
             * @private
